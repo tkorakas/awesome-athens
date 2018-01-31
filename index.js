f121679186e330a52de90f2f9a7cdaa7c078fbd5
Thanos Korakas <thanoskorakas@gmail.com>
2018-02-01 01:26:35 +0200

    Replaces ejs with nunjakes

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
-const ejs = require('ejs')
+const nunjucks = require('nunjucks')
 
 const groups = yaml.safeLoad(fs.readFileSync('groups.yml', 'utf8'))
 
-console.log(groups)
 let pages = fs.readdirSync(`pages/`)
 
-pages = pages.filter(page => page.includes('.ejs'))
+pages = pages.filter(page => page.includes('.njk'))
 
 pages.forEach((page) => {
-  ejs.renderFile(`pages/${page}`, {groups}, function(err, str) {
-    const filename = page.split('.')[0];
-    fs.closeSync(fs.openSync(`${filename}.html`, 'a'))
-    fs.writeFileSync(`${filename}.html`, str)
-  })
-})
+  const renderedPage = nunjucks.render(`pages/${page}`, {groups})
 
+  const filename = page.split('.')[0];
+  fs.closeSync(fs.openSync(`${filename}.html`, 'a'))
+  fs.writeFileSync(`${filename}.html`, renderedPage)
+})
--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const options = {
   valueNames: ['card-title', 'type']
 }
 
-// Refactor without jQuery.
 const hackerList = new List('cards', options)
 
 const resetButton = document.querySelector('#reset')
-const radios = document.querySelectorAll('input[name="inlineRadioOptions"]')
+const radios = document.querySelectorAll('input[name="radio"]')
 
-radios.forEach(el => {
-  el.addEventListener('change', (event) => {
+radios.forEach(function(el) {
+  el.addEventListener('change', function(event) {
     hackerList.filter(item => item.values().type === event.target.value)
   })
 })
+
 resetButton.addEventListener('click', function (e) {
   e.preventDefault();
   hackerList.filter(item => true)
   radios.forEach(el => el.checked = false);
-});
+})
