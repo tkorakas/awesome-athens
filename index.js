6dd8d98ad33c252b3e24f80c1fc35e768a21ba62
Thanos Korakas <thanoskorakas@gmail.com>
2018-02-06 23:50:26 +0200

    Completed main page and partials

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ pages = pages.filter(page => page.includes('.njk'))
 
 pageNames = pages.map(page => page.split('.')[0])
 
+const pageTitles = {
+  'index': 'Awesome Athens',
+  'about': 'About',
+  'contact': 'Contact'
+}
+
 if (!fs.existsSync('public')){
   fs.mkdirSync('public');
 }
@@ -17,7 +23,7 @@ if (!fs.existsSync('public')){
 pages.forEach((page) => {
   const currentPage = page.split('.')[0]
 
-  const renderedPage = nunjucks.render(`pages/${page}`, {groups, currentPage, pages: pageNames})
+  const renderedPage = nunjucks.render(`pages/${page}`, {groups, title: pageTitles[currentPage]})
 
   fs.closeSync(fs.openSync(`${__dirname}/public/${currentPage}.html`, 'a'))
   fs.writeFileSync(`${__dirname}/public/${currentPage}.html`, renderedPage)
